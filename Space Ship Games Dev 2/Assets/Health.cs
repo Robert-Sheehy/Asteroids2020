e5f816f1c8bee1596a25b535c88bc433c41df9c3
User <JP@itt.ie>
2020-11-10 15:58:35 +0000

    Signed-off-by: User <JP@itt.ie>

---
+++ Space Ship Games Dev 2/Assets/Health.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Health : MonoBehaviour
+{
+
+    internal int health = 100;
+
+    //TMPro Text health_Display;
+    // Start is called before the first frame update
+    void Start()
+    {
+     //   health_Display = gameObject.GetComponentinChildren<Text>(); 
+     //   health_Display.Text = health.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    internal void adjust_health(int adjustment)
+    {
+        health += adjustment;
+      //   health_Display.Text = health.ToString();
+        if (health<0)
+        Destroy(gameObject);
+    }
+}
