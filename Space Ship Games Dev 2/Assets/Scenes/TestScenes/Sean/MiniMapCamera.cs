2e486796a9e55391111e20a13a6db3d7a268bfa0
Sean Conway <Sean.Conway@students.ittralee.ie>
2020-11-19 12:59:27 +0000

    Signed-off-by: Sean Conway <Sean.Conway@students.ittralee.ie>

---
+++ Space Ship Games Dev 2/Assets/Scenes/TestScenes/Sean/MiniMapCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapCamera : MonoBehaviour
+{
+    private Transform PlayerTransform;
+    private float yoffset;
+
+  
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 targetPosition = PlayerTransform.position;
+        targetPosition.y += yoffset;
+        transform.position = targetPosition;
+    }
+}
